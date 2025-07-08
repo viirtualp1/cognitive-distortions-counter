f18535977e20e5045531fa46870572ded09c0089
virtualp1 <zininnikita309@gmail.com>
2025-07-09 00:10:24 +0400

    pwa init

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'cdcounter-cache-v1'
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icon-144x144.png',
+  '/icon-192x192.png',
+]
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cached) => cached || fetch(evt.request)),
+  )
+})
